cd61c91ce625a5a65c1dfbbd4e1354a8e43b7bfa
Joana R <48983248+joana-renteria@users.noreply.github.com>
2022-12-23 09:48:24 +0100

    Revert "initial commit for user's ORM"

--- storio/models/__init__.py
+++ storio/models/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import user
-from . import admin
-from . import client
-
 from . import item
 from . import model
--- storio/models/admin.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class admin(models.Model):
-
-    _inherit = 'storio.user'
-
-    isSuperAdmin = fields.Boolean(string='Is Super Admin', required=True)
