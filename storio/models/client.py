d73a0070d3d52293cf6ef455263ef79200ffc185
joana-renteria <joana.rente6@tuta.io>
2023-01-13 12:34:03 +0100

    start developing the model view

--- storio/models/client.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class Client(models.Model):
-
-    _inherit = 'storio.user'
-
-    #bookings = fields.One2many('storio.booking', 'booking_id', string='Client Bookings')
