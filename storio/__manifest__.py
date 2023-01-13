c924b35213760bf9b15208fe291a78a1fc43dae7
joana-renteria <joana.rente6@tuta.io>
2023-01-13 14:24:14 +0100

    menu

--- storio/__manifest__.py
+++ storio/__manifest__.py
@@ -24,7 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/models.xml',
+        'views/menu_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
