3235ff09028e50d56d91d45866a235922d7f6114
2dam <2dam@TartangaLH.eus>
2023-02-02 10:25:19 +0100

    onchange and constrais, but error with something

--- storio/models/pack.py
+++ storio/models/pack.py
@@ -3,6 +3,7 @@
 from odoo import api
 from odoo import fields
 from odoo import models
+from odoo.exceptions import ValidationError
 
 class pack(models.Model):
     _name = 'storio.pack'
@@ -11,14 +12,33 @@ class pack(models.Model):
     
     id = fields.Integer(string="Id", required=True)
     description = fields.Text(string="Description")
-    datePackAdd = fields.Date(string="Date Pack Added",required=True)
-    packState = fields.Selection(selection=[('Available', 'Available'), ('Unavailable', 'Unavailable')],string='Pack state')
+    datePackAdd = fields.Date(string="Date Pack Added", required=True)
+    packState = fields.Selection(selection=[('Available', 'Available'), ('Unavailable', 'Unavailable')], string='Pack state', required=True)
     packType = fields.Selection(selection=[
-                                    ('Sound', 'Sound'), 
-                                    ('Lighting', 'Lighting'), 
-                                    ('Camera', 'Camera'), 
-                                    ('Tripod', 'Tripod'),
-                                    ('Special', 'Special')],
-                                    string='Pack type')
+                                ('Sound', 'Sound'), 
+                                ('Lighting', 'Lighting'), 
+                                ('Camera', 'Camera'), 
+                                ('Tripod', 'Tripod'),
+                                ('Special', 'Special')],
+                                string='Pack type', required=True)
     #items = fields.One2many('storio.item', 'pack', string="Items")
-    bookings = fields.Many2Many('storio.booking', string="Bookings")
+    bookings = fields.Many2many('storio.booking', string="Bookings")
+
+    @api.onchange('description')
+    def _onchange_description(self):
+        if self.description:
+            if len(self.description) < 10:
+                return {
+                    'warning': {
+                        'title': "Incorrect 'description' value",
+                        'message': "value should atleast be 10 characters long"
+                    }
+                }
+
+    @api.multi
+    @api.constrains('name')
+    def _check_correct_name(self):
+        for r in self:
+            if len(self.name) < 5:
+                raise ValidationError("Name is not valid")
+            
