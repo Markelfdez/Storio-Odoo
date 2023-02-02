b7145995baf451ec7f9d35150584d1bfa47d3698
NicoRodBilbao <2002nikoniko2002@gmail.com>
2023-02-02 10:16:50 +0100

    Item now functional, security left

--- storio/models/item.py
+++ storio/models/item.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models,fields,api
+from odoo import models, fields, api, exceptions
 
 class Item(models.Model):
-     _name = 'storio.item'
+    _name = 'storio.item'
 
-     name = fields.Char()
+    name = fields.Char(default="Unknown", required=True)
      
-     iden = fields.Integer(string="ID", required=True)
-     issues = fields.Text(string="Issues")
-     dateAdded = fields.Date(string="Date Added",required=True)
-     model_id = fields.Many2one('storio.model',string="Model", required=True)
-     #pack = fields.Many2one('storio.pack',string="Pack")
+    iden = fields.Integer(string="ID", required=True, default="1")
+    issues = fields.Text(string="Issues", default="No issues.")
+    dateAdded = fields.Date(string="Date Added", required=True, default=fields.Date.today)
+    model_id = fields.Many2one('storio.model', string="Model", required=True)
      
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    @api.onchange('iden')
+    def _onchange_iden(self):
+        if self.iden <= 0:
+            
+            return {
+                'warning': {
+                    'title': "Incorrect ID value",
+                    'message': "ID cannot be negative.",
+                }
+            }   
+            
+    @api.constrains('iden')
+    def _constrains_iden(self):
+        if self.iden <= 0:
+            
+            raise exceptions.ValidationError("ID MAL.")
+            
