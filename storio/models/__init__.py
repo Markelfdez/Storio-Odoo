7f571befc209533258ff583302ce4f305cc9103f
2dam <2dam@TartangaLH.eus>
2023-02-02 08:26:48 +0100

    booking

--- storio/models/__init__.py
+++ storio/models/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from . import models
 from . import booking
+from . import item
+from . import model
+from . import pack
--- storio/models/booking.py
+++ storio/models/booking.py
@@ -5,15 +5,10 @@ from odoo import models, fields
 class Booking(models.Model):
     _name = 'storio.booking'
     
-    id = fields.Integer(string = "ID", required = true)
-    description = fields.Text(string = "Description", required = true)
-    endDate = fields.Date(string="End date", required = true)
-    startDate = fields.Date(string = "Start date", required = true)
-    user = fields.One2many(string = "User", required = true)
-    packs = fields.Many2many(string = "Packs", required = true)
+    id = fields.Integer(string = "ID", required = True)
+    description = fields.Text(string = "Description", required = True)
+    endDate = fields.Date(string="End date", required = True)
+    startDate = fields.Date(string = "Start date", required = True)
+    user = fields.One2many('res.users', string = "User", required = True)
+    packs = fields.Many2Many('storio.pack', string = "Packs", required = True)
     state = fields.Selection([("pending","PENDING"),("approved","APROVED"),("handed","HANDED"),("returned","RETURNED"),("denied","DENIED")], string = "State")
-    
-
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
