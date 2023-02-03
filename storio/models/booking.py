1302fbe9817407dbd408d58c41ce99ec31911ecd
NicoRodBilbao <2002nikoniko2002@gmail.com>
2023-02-03 01:19:41 +0100

    The module is now functional.(No users)

--- storio/models/booking.py
+++ storio/models/booking.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
-from odoo.exceptions import ValidationError
+from odoo import models, fields, api, exceptions
 
 
 class Booking(models.Model):
@@ -11,7 +10,7 @@ class Booking(models.Model):
     description = fields.Text(string = "Description", required = True)
     endDate = fields.Date(string="End date", default=fields.Date.from_string(fields.Date.today()), required = True)
     startDate = fields.Date(string = "Start date", default=fields.Date.from_string(fields.Date.today()), required = True)
-    user = fields.One2many('res.users', string = "User", required = True)
+    #user = fields.One2many('res.users', string = "User", required = True)
     packs = fields.Many2many('storio.pack', string = "Packs", required = True)
     state = fields.Selection([("pending","PENDING"),("approved","APROVED"),("handed","HANDED"),("returned","RETURNED"),("denied","DENIED")], string = "State")
 
@@ -23,12 +22,12 @@ class Booking(models.Model):
     @api.constrains('endDate')
     def _constrains_date(self):
         for task in self:
-            if fields.Date.from_string(task.endDate) > fields.Date.from_string(fields.Date.today()):
+            if fields.Date.from_string(task.endDate) < fields.Date.from_string(fields.Date.today()):
                 raise exceptions.ValidationError("Date cannot be greater than today date.")
 
     @api.onchange('endDate')
     def _onchange_date(self):
-        if fields.Date.from_string(self.endDate) > fields.Date.from_string(fields.Date.today()):
+        if fields.Date.from_string(self.endDate) < fields.Date.from_string(fields.Date.today()):
             return{
                 'warning':{
                     'title': "Date incorrect",
